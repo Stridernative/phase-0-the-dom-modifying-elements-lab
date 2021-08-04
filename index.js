2bdbcd57a6bbb46cbaf5c802743ea3cb807f649a
gabriel <gabecreacs@gmail.com>
2021-08-03 19:15:20 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector("main");
+    main.remove();
+
+
+const body = document.querySelector("body");
+body.innerHTML = "<h1 id='victory'>Gabe is the champion</h1>";
+const newHeader = document.querySelector("h1");
